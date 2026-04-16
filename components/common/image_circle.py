8b29b7d9b89486defaf6b0db244e37b4c70e2cc7
Lustiora <parkjaegun0219@gmail.com>
2026-04-17 03:15:29 +0900

    Add test data for pet_list and customer_food_detail. Update components and layout. Add mobile view scale logic. Create home_views_connect.py routing script.

--- components/common/image_circle.py
+++ components/common/image_circle.py
@@ -1,16 +1,19 @@
 import flet as ft
 
-def image_circle(event, size, src=None):
-    return ft.Container(
-        width=size,
-        height=size,
-        shape=ft.BoxShape.CIRCLE,
-        shadow=ft.BoxShadow(
+def image_circle(event, size, src=None, shadow=True): # type: ignore
+    if shadow:
+        image_shadow = ft.BoxShadow(
             spread_radius=2,
             blur_radius=20,
             color=ft.Colors.with_opacity(1, ft.Colors.OUTLINE_VARIANT),
             offset=ft.Offset(0, -4),
-        ),
+        )
+    else: image_shadow = None
+    return ft.Container(
+        width=size,
+        height=size,
+        shape=ft.BoxShape.CIRCLE,
+        shadow=image_shadow,
         on_click=event,
         image=ft.DecorationImage(src=src, fit=ft.BoxFit.COVER),
     )
--- components/layout/bottom_layout.py
+++ components/layout/bottom_layout.py
@@ -1,4 +1,5 @@
 import flet as ft
+import components as dogdog
 
 def continue_button(on_click=None):
     return ft.Container(
@@ -39,3 +40,54 @@ def bottom_appbar(appbar_button_list:list):
                 ft.Divider(height=1),
                 ft.Row(controls=appbar_button_list, expand=True, spacing=0)
     ]))
+
+def home_bottom_appbar(new_view, appbar_status, floating_btn_event):
+    def appbar_button(icon, text, on_click):
+        if icon is None and text is None:
+            return ft.Container(expand=True)
+        return ft.Container(
+            padding=0,
+            expand=True,
+            ink=True,
+            on_click=on_click,
+            content=ft.Column(
+                expand=True,
+                alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                spacing=3,
+                controls=[
+                    ft.Icon(icon=icon, color=ft.Colors.GREY_400, size=22),
+                    dogdog.basic_text(value=text, size=12, color=ft.Colors.GREY_400),
+                ],
+            ),
+        )
+
+    def appbar_floating_button(icon, on_click):
+        return ft.FloatingActionButton(
+            offset=ft.Offset(0, 0.2),
+            scale=1.3,
+            content=ft.Image(
+                src=icon,
+                fit=ft.BoxFit.COVER,
+            ),
+            bgcolor=ft.Colors.TRANSPARENT,
+            shape=ft.CircleBorder(),
+            elevation=0,
+            hover_elevation=0,
+            highlight_elevation=0,
+            focus_elevation=0,
+            splash_color=ft.Colors.TRANSPARENT,
+            hover_color=ft.Colors.TRANSPARENT,
+            focus_color=ft.Colors.TRANSPARENT,
+            on_click=on_click,
+        )
+    
+    def appbar_on_change(e, on_change_page):
+        floating_btn_event(f"/{on_change_page}") # type: ignore
+
+    appbar_button_list = [appbar_button(
+        icon=icon, text=text, on_click=on_click) for icon, text, on_click in appbar_status]
+    new_view.bottom_appbar = bottom_appbar(appbar_button_list=appbar_button_list)
+    new_view.floating_action_button = appbar_floating_button(
+        icon="skeleton.png", on_click=lambda e: appbar_on_change(e, "shop"))
+    new_view.floating_action_button_location = ft.FloatingActionButtonLocation.CENTER_DOCKED
